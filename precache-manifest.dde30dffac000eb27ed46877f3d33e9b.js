406e5eb426fc633cd19853a64071aa458bcc840f
ashishgupta2014 <ashish.2007g@gmail.com>
2020-04-12 13:21:56 +0530

    Updates

---
+++ precache-manifest.dde30dffac000eb27ed46877f3d33e9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c30fae62cd9b4d0040d58ad8bc2a7b7",
+    "url": "//monster-rolodex/index.html"
+  },
+  {
+    "revision": "37863935058a385d1148",
+    "url": "//monster-rolodex/static/css/main.6db3fcfa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "//monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37863935058a385d1148",
+    "url": "//monster-rolodex/static/js/main.8d1b0cca.chunk.js"
+  },
+  {
+    "revision": "8c2a227c83243889d7fc",
+    "url": "//monster-rolodex/static/js/runtime-main.183455a5.js"
+  }
+]);
